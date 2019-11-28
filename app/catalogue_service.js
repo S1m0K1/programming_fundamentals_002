4054b47f4689ad32ab26d0d14f2853ff2cd2d343
Simone Klein <simoneandpaper@gmail.com>
2019-11-28 12:18:55 +0000

    Attempts at Programming Fundamentals 002

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -21,26 +21,67 @@ const catalogue = [
   "Why Be Happy When You Could Be Normal? by Jeanette Winterson",
   "The Origin of Species by Charles Darwin"
 ];
-
-function countBooks() {
-  // Your code here
+function countBooks () {
+  return (catalogue.length);
 }
 
-function checkBook(book) {
-  // Your code here
+function checkBook (book) {
+  for(let i=0; i < catalogue.length; i++) {
+  if(catalogue[i] === book) {
+    return true;
+/* Works if replace i with x, but then would you need to use x, 
+not i throughout?! (See proceeding functions)*/
+  }
 }
 
-function countBooksByFirstLetter(letter) {
-  // Your code here
+return false;
 }
 
-function countBooksByKeyword(keyword) {
-  // Your code here
+function countBooksByFirstLetter (letter) {
+  let count = 0;
+// Is 0 the position you begin to count from?
+// So if let count = 5; for letter 'W' would you return 1? 
+  for(let i=0; i < catalogue.length; i++) {
+    if(catalogue[i][0] === letter) {
+/* Help! catalogue[i] is a row in the catalogue[array],
+a letter here === position 0 in a catalogue row: catalogue[i]*/
+      count++;
+// count++ Does this mean tally up the counts of letter === catlogue[1][0]?
+    }
+  }
+  return count;
 }
 
-function getBooksByAuthor(author) {
-  // Your code here
+function countBooksByKeyword (keyword) {
+  let count = 0;
+  for(let i=0; i < catalogue.length; i++) {
+    if(catalogue[i].toLowerCase().includes(keyword.toLowerCase())) {
+/* ??? 
+1) catalogue[i].toLowerCase() = makes row in catalogue all lower case? 
+What is ()? Is it placeholder for argument?
+2) If the lowercase row in catalogue.includes:
+3) keyword.toLowerCase() = changes whatever keyword is input to lowercase?
+*/ 
+
+// if(catalogue[i].toLowerCase() === keyword) 
+// this wouldn't work because here keyword === entire row of catalogue?
+      count++;
+    }
+  }
+  return count;
 }
+
+function getBooksByAuthor (author) {
+  let books = [];
+  for(let i=0; i < catalogue.length; i++) {
+    if(catalogue[i].toLowerCase().includes(author.toLowerCase())) {
+      books.push(catalogue[i]);
+// ??? Push matched books to a new array?
+          }
+        }
+        return books;
+// What do you want to return? Books, Number (count)
+      }
 
 module.exports = {
   countBooks,
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,9 +1,45 @@
 const catalogueService = require("../app/catalogue_service");
-
 describe("catalogueService", () => {
-  describe("catalogueService.countBooks", () => {
-    test("returns the number of books in the list", () => {
-      expect(catalogueService.countBooks()).toBe(20);
-    });
+ describe("catalogueService.countBooks", () => {
+   test("returns the number of books in the list", () => {
+     expect(catalogueService.countBooks()).toBe(20);
+   });
+ });
+
+  describe("catalogueService.checkBook", () => {
+    test("returns true if the book exists in the list", () => {
+      expect(catalogueService.checkBook("Dracula by Bram Stoker")).toBe(true);
   });
+});
+
+    test("returns false if the book doesn't exist in the list", () => {
+      expect(catalogueService.checkBook("Moths by Pamela Mothman")).toBe(false);
+  });
+});
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns the number of books beginning with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+  });
+});
+
+    test("returns 0 if no books begin with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("X")).toBe(0);
+  });
+
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns the number of how many book titles match a given keyword", () => {
+      expect(catalogueService.countBooksByKeyword("assassin")).toBe(3); // returns 3
+      expect(catalogueService.countBooksByKeyword("normal")).toBe(2); // returns 2
+    });
+
+  describe("catalogueService.getBooksByAuthor", () => {
+    test ("returns all books by a particular author", () => { 
+// What are the empty () for?
+      expect(catalogueService.getBooksByAuthor("Charles Dickens")).toEqual([
+      "A Tale of Two Cities by Charles Dickens",
+      "Oliver Twist by Charles Dickens",
+      "Great Expectations by Charles Dickens"]);  
+  });
+});
 });
